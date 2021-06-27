ef10b4bfea9e2ea6ce630e9eb7bd1fe52da44056
afsanaeva <afsanaeva05@gmail.com>
2021-06-27 21:11:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+document.getElementById("searchBtn").addEventListener("click", function () {
+  const locationInput = document.getElementById("locationInput");
+  const location = document.getElementById("locationAddress");
+  const temperature = document.getElementById("temperatureAmount");
+  const weatherType = document.getElementById("weatherType");
+
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=291d5ce696b82568e271736ceb0d0588')
+    .then (res => res.json())
+    .then(data => {
+    console.log(data)
+    locationInput.innerText = data.name;
+    let temperatureCelsius = Math.round(data.main.temp -273.15) ;//Temperature Convert Kelvin to Celsius
+    temperature.innerText = temperatureCelsius;
+    weatherType.innerText =data.weather.main[0];
+    }) 
+  })
+
+
+  // {"coord":{"lon":90.4074,"lat":23.7104},
+  // "weather":[{"id":721,"main":"Haze","description":"haze","icon":"50n"}],
+  // "base":"stations",
+  // "main":{"temp":304.14,"feels_like":310.72,"temp_min":304.14,"temp_max":304.14,"pressure":1006,"humidity":70},
+  // "visibility":4000,
+  // "wind":{"speed":5.14,"deg":170},
+  // "clouds":{"all":75},
+  // "dt":1624805059,
+  // "sys":{"type":1,"id":9145,
+  // "country":"BD",
+  // "sunrise":1624749216,
+  // "sunset":1624798139},
+  // "timezone":21600,
+  // "id":1185241,
+  // "name":"Dhaka",
+  // "cod":200}
+
+
+  
